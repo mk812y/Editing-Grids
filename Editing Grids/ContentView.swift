cbdf3d23e365e0dadbe040b14c598817f543e029
mk812y <mk812y@gmail.com>
2023-09-14 21:15:51 +0200

    Initial Commit

---
+++ Editing Grids/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Editing Grids
+//
+//  Created by Михаил Куприянов on 14.9.23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Editing Grids/Editing_GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Editing_GridsApp.swift
+//  Editing Grids
+//
+//  Created by Михаил Куприянов on 14.9.23..
+//
+
+import SwiftUI
+
+@main
+struct Editing_GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
