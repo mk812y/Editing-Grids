83d03daf0378efb5188c8359a6f74e6ea92ae953
mk812y <mk812y@gmail.com>
2023-09-15 15:23:12 +0200

    SymbolPicker

---
+++ Editing Grids/SymbolPicker.swift
@@ -0,0 +1,18 @@
+//
+//  SymbolPicker.swift
+//  Editing Grids
+//
+//  Created by Михаил Куприянов on 15.9.23..
+//
+
+import SwiftUI
+
+struct SymbolPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SymbolPicker()
+}
